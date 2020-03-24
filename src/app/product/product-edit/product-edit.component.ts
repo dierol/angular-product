4f0b0fac771510b6425a3c819ba08bf397029e57
dierol <diegorolle90@gmail.com>
2020-03-24 12:46:20 +0100

    edit/add done

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { ProductService } from '../product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+  product: Product = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0
+  };
 
-  constructor() { }
+  label: string = 'Add Product';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: ProductService
+  ) {};
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      let id = params.get('id');
+      if (parseInt(id) > 0) {
+        this.label = 'Edit Product';
+        this.service.get(id).subscribe(response => this.product = response);
+      }
+    });
+  };
+
+  submit() {
+      console.log(this.service);
+      let id = this.product.id;
+      if (id > 0) {
+          this.service.put(id, this.product).subscribe(response => this.router.navigate(['/products']));
+      } else {
+          this.service.post(this.product).subscribe(response => this.router.navigate(['/products']));
+      }
   }
 
 }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Product } from '../product';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'none'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  list() {
-    return this.http.get<Product[]>('http://localhost:3000/posts');
+  list(): Observable<Product[]> {
+    return this.http.get<Product[]>('http://localhost:3000/products');
   }
 
-  get(id: number | string) {
-    return this.http.get<Product>('http://localhost:3000/posts/'+id);
+  get(id: number | string): Observable<Product> {
+    return this.http.get<Product>('http://localhost:3000/products/'+id);
+  }
+
+  post(product: Product): Observable<Product> {
+    return this.http.post<Product>('http://localhost:3000/products/', product, httpOptions);
+  }
+
+  put(id: number | string, product: Product): Observable<Product> {
+    return this.http.put<Product>('http://localhost:3000/products/'+id, product, httpOptions);
   }
 }
